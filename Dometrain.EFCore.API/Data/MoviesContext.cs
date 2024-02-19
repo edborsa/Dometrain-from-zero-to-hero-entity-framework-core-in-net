ca65baeb93b754254d77781d6715c4f4e8ffb8ea
Eduardo Borsa <eborsa@loomissayles.com>
2024-02-19 15:54:59 -0300

    End of Seeing what goes on in the Database
    Signed-off-by: Eduardo Borsa <eborsa@loomissayles.com>

--- Dometrain.EFCore.API/Data/MoviesContext.cs
+++ Dometrain.EFCore.API/Data/MoviesContext.cs
@@ -10,7 +10,14 @@ public class MoviesContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(
-            "Data Source=localhost;Initial Catalog=MoviesDB;User ID=sa;Password=docker_123;TrustServerCertificate=True");
+            """
+            Data Source=localhost;
+            Initial Catalog=MoviesDB;
+            User ID=sa;
+            Password=docker_123;
+            TrustServerCertificate=True
+            """);
+        optionsBuilder.LogTo(Console.WriteLine);
         base.OnConfiguring(optionsBuilder);
     }
 }
