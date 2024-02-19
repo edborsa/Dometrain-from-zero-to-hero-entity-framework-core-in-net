2bcfe8548bc9030f0a8dfd673ca3dd9bbe19a4ac
Eduardo Borsa <eborsa@loomissayles.com>
2024-02-19 07:19:23 -0300

    End Of Getting the Model to work in our application
    Signed-off-by: Eduardo Borsa <eborsa@loomissayles.com>

--- Dometrain.EFCore.API/Data/MooviesContext.cs
+++
@@ -1,9 +0,0 @@
-using Dometrain.EFCore.API.Models;
-using Microsoft.EntityFrameworkCore;
-
-namespace Dometrain.EFCore.API.Data;
-
-public class MooviesContext : DbContext
-{
-    public DbSet<Movie> Movies => Set<Movie>();
-}
---
+++ Dometrain.EFCore.API/Data/MoviesContext.cs
@@ -0,0 +1,16 @@
+using Dometrain.EFCore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dometrain.EFCore.API.Data;
+
+public class MoviesContext : DbContext
+{
+    public DbSet<Movie> Movies => Set<Movie>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Data Source=localhost;Initial Catalog=MoviesDB;User ID=sa;Password=docker_123;TrustServerCertificate=True");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
